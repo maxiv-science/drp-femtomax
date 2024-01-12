59e7892f1c58325c1f004461f5de56b3794d790a
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2024-01-12 17:51:04 +0100

    integration

--- src/reducer.py
+++ src/reducer.py
@@ -1,7 +1,7 @@
 import logging
 
 from dranspose.event import ResultData
-from dranspose.parameters import StrParameter
+from dranspose.parameters import StrParameter, BoolParameter
 import os
 import h5py
 import numpy as np
@@ -13,47 +13,69 @@ class CmosReducer:
     def describe_parameters():
         params = [
             StrParameter(name="filename"),
+            BoolParameter(name="integrate"),
         ]
         return params
 
     def __init__(self, parameters=None, **kwargs):
-        self.publish = {"hits": {}}
-        try:
-            filename = parameters["filename"].data
-        except:
-            filename = None
-        size = int(parameters["spot_size"].data)
-        logger.info("writing to file %s", filename)
-        self.dset = None
-        if filename:
-            if os.path.isfile(filename):
-                self._fh = h5py.File(filename, 'a')
-                self.offsetdset = self._fh.get("sparse/offset")
-                self.dset = self._fh.get("sparse/data")
+        if parameters["analysis_mode"].data == b"roi":
+            self.publish = {"last": np.zeros((100,100)), "cropped": None, "nint": 0}
+        elif parameters["analysis_mode"].data == b"sparsification":
+            self.publish = {"hits": {}}
+            try:
+                filename = parameters["filename"].data
+            except:
+                filename = None
+            size = int(parameters["spot_size"].data)
+            logger.info("writing to file %s", filename)
+            self.dset = None
+            if filename:
+                if os.path.isfile(filename):
+                    self._fh = h5py.File(filename, 'a')
+                    self.offsetdset = self._fh.get("sparse/offset")
+                    self.dset = self._fh.get("sparse/data")
+                else:
+                    self._fh = h5py.File(filename, 'w')
+                    group = self._fh.create_group("sparse")
+                    self.offsetdset = group.create_dataset("offset", (0,3), maxshape=(None,3), dtype=np.int16)
+                    self.dset = group.create_dataset("data", (0,size, size), maxshape=(None,size, size), dtype=np.uint16)
+                    self._offset_dset_name = f"sparse/offset"
+                logger.info("opened file at %s", self._fh)
             else:
-                self._fh = h5py.File(filename, 'w')
-                group = self._fh.create_group("sparse")
-                self.offsetdset = group.create_dataset("offset", (0,3), maxshape=(None,3), dtype=np.int16)
-                self.dset = group.create_dataset("data", (0,size, size), maxshape=(None,size, size), dtype=np.uint16)
-                self._offset_dset_name = f"sparse/offset"
-            logger.info("opened file at %s", self._fh)
-        else:
-            self._fh = None
+                self._fh = None
 
     def process_result(self, result: ResultData, parameters=None):
-        if result.payload:
-            self.publish["hits"][result.event_number] = result.payload
-            if self.dset:
-                nhits = result.payload["offsets"].shape[0]
-                oldsize = self.dset.shape[0]
-                self.dset.resize(oldsize + nhits, axis=0)
-                self.offsetdset.resize(oldsize + nhits, axis=0)
+        if parameters["analysis_mode"].value == "roi":
+            if result.payload:
+                img = result.payload["img"]
+                cropped = result.payload["cropped"]
+                if parameters["integrate"].value:
+                    if self.publish["last"].shape != img.shape or cropped != self.publish["cropped"]:
+                        self.publish["last"] = img
+                        self.publish["nint"] = 1
+                    else:
+                        self.publish["last"] = self.publish["last"] + img
+                        self.publish["nint"] += 1
+                else:
+                    self.publish["last"] = img
+                    self.publish["nint"] = 1
+                self.publish["cropped"] = cropped
 
-                self.dset[oldsize:oldsize+nhits,:,:] = result.payload["spots"]
-                self.offsetdset[oldsize:oldsize+nhits,:] = result.payload["offsets"]
-                logger.debug("written record")
+        elif parameters["analysis_mode"].data == b"sparsification":
+            if result.payload:
+                self.publish["hits"][result.event_number] = result.payload
+                if self.dset:
+                    nhits = result.payload["offsets"].shape[0]
+                    oldsize = self.dset.shape[0]
+                    self.dset.resize(oldsize + nhits, axis=0)
+                    self.offsetdset.resize(oldsize + nhits, axis=0)
+
+                    self.dset[oldsize:oldsize+nhits,:,:] = result.payload["spots"]
+                    self.offsetdset[oldsize:oldsize+nhits,:] = result.payload["offsets"]
+                    logger.debug("written record")
 
     def finish(self, parameters=None):
+        print(self.publish)
         logger.info("finished reducer custom")
         if self._fh:
             self._fh.close()
--- src/worker.py
+++ src/worker.py
@@ -2,7 +2,7 @@ import logging
 import tempfile
 
 from dranspose.event import EventData
-from dranspose.parameters import IntParameter, FileParameter
+from dranspose.parameters import IntParameter, StrParameter
 from dranspose.middlewares.stream1 import parse
 from dranspose.data.stream1 import Stream1Data
 import numpy as np
@@ -19,6 +19,7 @@ class CmosWorker:
             IntParameter(name="background"),
             IntParameter(name="threshold"),
             IntParameter(name="spot_size"),
+            StrParameter(name="analysis_mode", default="roi"),
         ]
         return params
 
@@ -26,34 +27,44 @@ class CmosWorker:
         self.number = 0
 
     def process_event(self, event: EventData, parameters=None):
-        print("got event", event)
-        logger.debug("using parameters %s", parameters)
-        bg = int(parameters["background"].data)
-        thr = int(parameters["threshold"].data)
-        size = int(parameters["spot_size"].data)
-        dat = parse(event.streams["andor3_balor"])
-        if not isinstance(dat, Stream1Data):
-            return
-        dark_corr = dat.data.clip(min=bg) - bg
-        frame = dat.frame
-        logger.debug("frameno %d", frame)
-        positions = np.asarray(dark_corr > thr).nonzero()
-        hits = set()
-        for x, y in zip(*positions):
-            small = dark_corr[x - 1:x + 2, y - 1:y + 2]
-            maxpos = unravel_index(small.argmax(), small.shape)
-            hits.add((x + maxpos[0] - 1, y + maxpos[1] - 1))
 
-        logger.debug("found %d hits", len(hits))
-        spots = np.zeros((len(hits), size,size))
-        pad = int(np.ceil(size/2))
-        padded = np.pad(dark_corr, pad)
-        offsets = np.zeros((len(hits), 3), dtype=np.int16)
-        for i, (x,y) in enumerate(hits):
-            spot = padded[pad + x - 3:pad + x + 4, pad + y - 3:pad + y + 4]
-            spots[i] = spot
-            offsets[i] = [frame, x - 3, y - 3]
-        return {"spots": spots, "offsets": offsets}
+        if parameters["analysis_mode"].value == "roi":
+            if "andor3_balor" in event.streams:
+                dat = parse(event.streams["andor3_balor"])
+                bg = parameters["background"].value
+                if not isinstance(dat, Stream1Data):
+                    return
+                return {"img": dat.data.clip(min=bg) - bg , "cropped": None}
+
+        elif parameters["analysis_mode"].data == b"sparsification":
+            logger.debug("using parameters %s", parameters)
+            bg = int(parameters["background"].data)
+            thr = int(parameters["threshold"].data)
+            size = int(parameters["spot_size"].data)
+            dat = parse(event.streams["andor3_balor"])
+            if not isinstance(dat, Stream1Data):
+                return
+            dark_corr = dat.data.clip(min=bg) - bg
+            pad = int(np.ceil(size / 2))
+            padded = np.pad(dark_corr, pad)
+
+            frame = dat.frame
+            logger.debug("frameno %d", frame)
+            positions = np.asarray(padded > thr).nonzero()
+            hits = set()
+            for x, y in zip(*positions):
+                small = padded[x - 1:x + 2, y - 1:y + 2]
+                maxpos = unravel_index(small.argmax(), small.shape)
+                hits.add((x + maxpos[0] - 1, y + maxpos[1] - 1))
+
+            logger.debug("found %d hits", len(hits))
+            spots = np.zeros((len(hits), size,size))
+            offsets = np.zeros((len(hits), 3), dtype=np.int16)
+            for i, (x,y) in enumerate(hits):
+                spot = padded[pad + x - 3:pad + x + 4, pad + y - 3:pad + y + 4]
+                spots[i] = spot
+                offsets[i] = [frame, x - 3, y - 3]
+            return {"spots": spots, "offsets": offsets}
 
     def finish(self, parameters=None):
         print("finished")
