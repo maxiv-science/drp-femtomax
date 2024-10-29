aeeb8ad7450d5c4667e07b63815ced844824c3bd
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2024-10-29 16:27:18 +0100

    asserts

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,16 +1,39 @@
+import asyncio
 import logging
 import threading
-from glob import glob
+import time
 
 import h5pyd
 from dranspose.replay import replay
 
 
-def test_replay_gen():
-    replay(
-        "src.worker:CmosWorker",
-        "src.reducer:CmosReducer",
-        None,
-        "src.saved_source:BalorSource",
-        "testparams.json",
+def test_pipeline():
+    stop_event = threading.Event()
+    done_event = threading.Event()
+
+    thread = threading.Thread(
+        target=replay,
+        args=(
+            "src.worker:CmosWorker",
+            "src.reducer:CmosReducer",
+            None,
+            "src.saved_source:BalorSource",
+            "testparams.json",
+        ),
+        kwargs={"port": 5010, "stop_event": stop_event, "done_event": done_event},
     )
+    thread.start()
+
+    # do live queries
+
+    done_event.wait()
+
+    f = h5pyd.File("http://localhost:5010/", "r")
+    logging.info("file %s", list(f.keys()))
+
+    assert f["nint"][()] == 21
+    assert f["last"][10, 10] == 21
+
+    stop_event.set()
+
+    thread.join()
